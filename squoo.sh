881f48bfbabab7cb261c38e247c34f960eca13cf
entr0pia <30486766+entr0pia@users.noreply.github.com>
2021-12-09 15:13:29 +0800

    add msgs

--- squoo.sh
+++ squoo.sh
@@ -1,9 +1,13 @@
 #!/system/bin/sh
 
-# t=`cat trilibs.txt | grep -oP '\d{9}' | sort -n | sed '$d'`
-t=`find /data/app -name  'com.google.android.trichromelibrary*' | grep -oE '[0-9]{9}' | sort -n | sed '$d'`
+# t=`cat trilibs.txt | grep -oP '\d{9}' | sort -n`
+t=`find /data/app -name  'com.google.android.trichromelibrary*' | grep -oE '[0-9]{9}' | sort -n`
+
+echo find trichromelibrary versions: $t
+
+t=`echo "$t" | sed '$d'`
 
 for i in $t; do
-	echo "uninstalling: com.google.android.trichromelibrary_$i"
+    echo "uninstalling: com.google.android.trichromelibrary_$i"
     pm uninstall com.google.android.trichromelibrary_$i
 done
