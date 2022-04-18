5cefae192125c6353b0d232115d893e88836287c
entr0pia <30486766+entr0pia@users.noreply.github.com>
2022-04-18 15:28:56 +0800

    No delay when running as shell

--- service.sh
+++ service.sh
@@ -12,14 +12,18 @@ function squoosh_libs() {
     echo "Removing $lib"
     pm uninstall $lib
   done
+  echo "If you see any failure messages, just ignore them, because the TrichromeLibrary is being used."
 }
 
-sleep 15
+
+if [ "$(whoami)" != "shell" ]; then
+  echo "19 seconds delay to let the system finish booting..."
+  sleep 19
+fi
 
 webview_stat=$(dumpsys webviewupdate)
 current_ver=$(echo "$webview_stat" | grep 'Current WebView package' | grep -oE '[0-9\.]{2,}')
 current_code=$(echo "$webview_stat" | grep "$current_ver" | grep -oE '[0-9]{9}')
 
-tri_libs=$(dumpsys package|grep name:com.google.android.trichromelibrary | sed "s/ version:/_/g; s/name://g; /$current_code/d")
+tri_libs=$(dumpsys -t 1 package | grep name:com.google.android.trichromelibrary | sed "s/[ ]version:/_/g; s/name://g; /$current_code/d")
 squoosh_libs $tri_libs | tee "/data/local/tmp/trichromelibrary-squoosh.log"
-echo "If you see any failure messages, just ignore them, because the TrichromeLibrary is being used in other ways."
